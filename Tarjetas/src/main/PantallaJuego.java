c8f83b62639a331606fecdd8acbe36c0d6ea7916
Santiago Fuentes Cruz <santiagofu96@gmail.com>
2020-04-06 16:18:50 +0200

    Fixed starvation on laws randomizer.

--- Tarjetas/src/main/PantallaJuego.java
+++ Tarjetas/src/main/PantallaJuego.java
@@ -12,34 +12,33 @@ import javax.swing.JTextArea;
 import tarjetaPckg.Tarjeta;
 
 public class PantallaJuego {
-	
+
 	private static String texto;
 
 	public static void jugarTarjetas(Tarjeta[] listado) {
-		
+
 		JFrame frmJuegoTarjetas = new JFrame();
 		frmJuegoTarjetas.getContentPane().setBackground(new Color(55, 184, 219));
 		frmJuegoTarjetas.setTitle("Tarjetas de Leyes");
 		frmJuegoTarjetas.setBounds(500, 250, 700, 500);
 		frmJuegoTarjetas.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frmJuegoTarjetas.getContentPane().setLayout(null);
-		
+
 		JTextArea lbl = new JTextArea("");
 		lbl.setEditable(false);
 		lbl.setLineWrap(true);
 		lbl.setWrapStyleWord(true);
 		lbl.setBackground(new Color(55, 184, 219));
-		lbl.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lbl.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lbl.setBounds(200, 100, 300, 150);
 		frmJuegoTarjetas.getContentPane().add(lbl);
-		
-		
+
 		JButton btnSiguiente = new JButton("SIGUIENTE");
 		btnSiguiente.setBackground(Color.LIGHT_GRAY);
 		btnSiguiente.setFont(new Font("Arial", Font.BOLD, 20));
 		btnSiguiente.setBounds(50, 350, 200, 50);
 		frmJuegoTarjetas.getContentPane().add(btnSiguiente);
-		
+
 		JButton btnSolucionar = new JButton("SOLUCIONAR");
 		btnSolucionar.setBackground(Color.LIGHT_GRAY);
 		btnSolucionar.setFont(new Font("Arial", Font.BOLD, 20));
@@ -47,26 +46,35 @@ public class PantallaJuego {
 		frmJuegoTarjetas.getContentPane().add(btnSolucionar);
 		Tarjeta inicial = TriggerJugar.CargarPrimeraTarjeta();
 		lbl.setText(inicial.getAnverso().toString());
-		
+
 		texto = lbl.getText();
-		
+
 		frmJuegoTarjetas.setVisible(true);
-		
+
 		btnSiguiente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				texto = lbl.getText();
+				btnSiguiente.setEnabled(false);
+				btnSolucionar.setEnabled(false);
 				Tarjeta siguiente = TriggerJugar.Siguiente();
-				
+				lbl.setText("");
+				lbl.setText(siguiente.getAnverso());
+				btnSiguiente.setEnabled(true);
+				btnSolucionar.setEnabled(true);
 			}
 		});
-		
+
 		btnSolucionar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				btnSiguiente.setEnabled(false);
+				btnSolucionar.setEnabled(false);
 				texto = lbl.getText();
 				Tarjeta solucion = TriggerJugar.Solucionar(texto);
-				
+				lbl.setText("");
+				lbl.setText(solucion.getReverso());
+				btnSiguiente.setEnabled(true);
+				btnSolucionar.setEnabled(true);
 			}
 		});
-		
+
 	}
 }
--- Tarjetas/src/main/TriggerJugar.java
+++ Tarjetas/src/main/TriggerJugar.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -8,8 +9,9 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import tarjetaPckg.Tarjeta;
 
 public class TriggerJugar {
-	
+
 	private static Tarjeta[] listado;
+	private static ArrayList<Tarjeta> mostrados;
 
 	public static void Jugar() {
 		try {
@@ -25,27 +27,41 @@ public class TriggerJugar {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
-	
+
 	public static Tarjeta CargarPrimeraTarjeta() {
 		// TODO Auto-generated method stub
+		mostrados = new ArrayList<Tarjeta>();
 		int index = new Random().nextInt(listado.length);
-		return listado[index];
+		Tarjeta primera = listado[index];
+		mostrados.add(primera);
+		return primera;
 	}
-	
+
 	public static Tarjeta Siguiente() {
-		return null;
-		
+		if (mostrados.size() == listado.length) {
+			mostrados.clear();
+		}
+		int index = new Random().nextInt(listado.length);
+
+		while (mostrados.contains(listado[index])) {
+			index = new Random().nextInt(listado.length);
+		}
+
+		mostrados.add(listado[index]);
+		return listado[index];
+
 	}
-	
+
 	public static Tarjeta Solucionar(String texto) {
+		for (Tarjeta t : listado) {
+			if (t.getAnverso().compareTo(texto) == 0) {
+				return t;
+			}
+		}
 		return null;
-		
-		
-	}
-
 
 	}
 
+}
